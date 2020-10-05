24967a4b38ab8d78e90ee11f88e843d49ec2c236
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2020-10-05 18:42:02 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,9 +16,7 @@ sudo apt-get update
 sudo apt install curl
 nvm install 12.18.4
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 sudo npm install -g @vue/cli
 npm install
