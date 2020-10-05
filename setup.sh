48ee424b1fe7766462a4ba1c8ec9aa2ee7599107
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2020-10-05 16:16:25 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
-
 # Setup backend
 echo 'Setting up backend...'
 python3 -m venv ./backend/env
 source ./backend/env/bin/activate
 pip3 install -r backend/requirements.txt
+deactivate
 
 # Setup frontend
 echo 'Setting up frontend...'
 cd frontend
-sudo apt install nodejs
-sudo npm install -g @vue/cli
 npm install
 cd ..
 
@@ -18,9 +16,5 @@ echo 'Installing nginx'
 sudo apt-get update
 sudo apt-get install nginx
 
-deactivate
-
 # Done
 echo 'Done, see README.md for more details'
-
-
