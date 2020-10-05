d29767101790ab327bf52bee17d5aed358d448a2
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2020-10-05 17:10:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Install Basics
-sudo apt-get install python3-venv
-sudo apt install python3-pip
+python3 -m venv env
+sudo apt-get install python3-pip
 
 # Setup backend
 echo 'Setting up backend...'
