ae1cb1d4e1cccc2eaa459385c8a26cbb016e2c6a
LeonidasVarveropoulos <55664403+LeonidasVarveropoulos@users.noreply.github.com>
2020-10-05 18:57:02 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,11 +14,11 @@ echo 'Setting up frontend...'
 cd frontend
 sudo apt-get update
 sudo apt install curl
-nvm install 12.18.4
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 12.18.4
 
 sudo npm install -g @vue/cli
 npm install
